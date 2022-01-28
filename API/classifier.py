70edb9d53d05b1c3be9e1c38d58f86770e403346
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2022-01-28 15:28:03 +0530

    Update classifier.py

--- API/classifier.py
+++ API/classifier.py
@@ -16,14 +16,4 @@ X_test_scaled = X_test/255.0
 clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
 
 def get_prediction(image):
-    im_pil = Image.open(image)
-    image_bw = im_pil.convert('L')
-    image_bw_resized = image_bw.resize((28,28), Image.ANTIALIAS)
-    pixel_filter = 20
-    min_pixel = np.percentile(image_bw_resized, pixel_filter)
-    image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
-    max_pixel = np.max(image_bw_resized)
-    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
-    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
-    test_pred = clf.predict(test_sample)
-    return test_pred[0]
+ 
